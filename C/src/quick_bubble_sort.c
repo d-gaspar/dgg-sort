19f0931e8e8251b4bdf3bcc4fd4e6e0c232d5538
Daniel Gaspar Gonçalves <52671074+d-gaspar@users.noreply.github.com>
2019-08-11 09:39:03 -0300

    Update quick_bubble_sort.c

--- C/src/quick_bubble_sort.c
+++ C/src/quick_bubble_sort.c
@@ -6,43 +6,37 @@
 // #                                                                   #
 // #####################################################################
 
-void quick_sort_modified(int *arr, int pos1, int pos2, int array_cut);
 void quick_bubble_sort(int *arr, int pos1, int pos2, int array_cut);
+void quick_sort_modified(int *arr, int pos1, int pos2, int array_cut);
 
-void print_array(int arr[], int pos1, int pos2){
-    int i;
-    pos1 = 0;
-    pos2 = array_n-1;
-    int n_half = array_n/2;
+void quick_bubble_sort(int *arr, int pos1, int pos2, int array_cut){
 
-    if(array_n % 2 == 0){
-        for(i=pos1; i<=pos2; i++){
-            if(i == n_half){
-                printf(" |");
-            }
-            printf(" %d", arr[i]);
-        }
+    int i, j;
+
+    if((pos2-pos1) <= array_cut){
+        // Single Quick sort
+        quick_sort_modified(arr, pos1, pos2, array_cut);
     } else {
-        for(i=pos1; i<=pos2; i++){
-            if(i == n_half){
-                printf(" | %d |", arr[i]);
-            } else {
-                printf(" %d", arr[i]);
-            }
-        }
+        // Quick sort
+        quick_sort_modified(arr, pos1, pos2, array_cut);
     }
 
-    printf("\n");
+    // Bubble
+    i = 0;
+    while(i < pos2){
+
+        if(arr[i] > arr[i+1]){
+            j = i;
+            while(j>=0 && arr[j] > arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+
+                j--;
+            }
+        }
+        i++;
+
+    }
 }
-
-void swap(int *a, int *b){
-    int aux;
-
-    aux = *a;
-    *a = *b;
-    *b = aux;
-}
-
 
 void quick_sort_modified(int *arr, int pos1, int pos2, int array_cut){
 
@@ -76,31 +70,3 @@ void quick_sort_modified(int *arr, int pos1, int pos2, int array_cut){
     }
 }
 
-void quick_bubble_sort(int *arr, int pos1, int pos2, int array_cut){
-
-    int i, j;
-
-    if((pos2-pos1) <= array_cut){
-        // Single Quick sort
-        quick_sort_modified(arr, pos1, pos2, array_cut);
-    } else {
-        // Quick sort
-        quick_sort_modified(arr, pos1, pos2, array_cut);
-    }
-
-    // Bubble
-    i = 0;
-    while(i < pos2){
-
-        if(arr[i] > arr[i+1]){
-            j = i;
-            while(j>=0 && arr[j] > arr[j+1]){
-                swap(&arr[j], &arr[j+1]);
-
-                j--;
-            }
-        }
-        i++;
-
-    }
-}
