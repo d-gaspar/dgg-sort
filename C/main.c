e9a38e1d25fbd5847c80841397d5db89e8c05826
Daniel Gaspar Gonçalves <52671074+d-gaspar@users.noreply.github.com>
2019-08-11 09:41:23 -0300

    Update main.c

--- C/main.c
+++ C/main.c
@@ -9,246 +9,156 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <direct.h>
 
 int array_n = 1000;
-int n_arrays = 100;
 int repeats = 100;
-int array_opt = 4;
+int array_opt = 0; // opts (0=Sorted; 1=Random; 2=Nearly_sorted; 3=Reverse; 4=Few_unique)
 
 int array_cut = 3000; // Quick Bubble sort
 
-#include "src/quick_bubble_sort.c"
+#include "src/general_functions.c"
 #include "src/dgg_sort.c"
+#include "src/quick_bubble_sort.c"
 #include "src/quick_sort.c"
-
-int *generate_array(int n, int opt){
-    int i, j, aux, aux2=3, *arr;
-
-    arr = (int *) calloc(n, sizeof(int));
-
-    switch(opt){
-        // Sorted
-        case 0:
-            for(i=0; i<n; i++){
-                arr[i] = i+1;
-            }
-
-            break;
-
-        // Random
-        case 1:
-            // Inicialize
-            for(i=0; i<n; i++){
-                arr[i] = i+1;
-            }
-
-            // Fisher-Yates shuffle
-            for(i=1; i<(n-1); i++){
-                j = rand() % i;
-
-                // Swap
-                aux = arr[i];
-                arr[i] = arr[j];
-                arr[j] = aux;
-            }
-
-            break;
-
-        // Nearly sorted
-        case 2:
-            // Inicialize
-            for(i=0; i<n; i++){
-                arr[i] = i + 1;
-            }
-
-            if(aux2 < n){
-                // Fisher-Yates shuffle
-                for(i=1; i<aux2; i++){
-                    j = rand() % i;
-
-                    // Swap
-                    aux = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = aux;
-                }
-            }
-
-            break;
-
-        // Reverse
-        case 3:
-            j = n;
-
-            for(i=0; i<n; i++){
-                arr[i] = j;
-                j = j - 1;
-            }
-
-            break;
-
-        // Few unique
-        case 4:
-            // Inicialize
-            for(i=0; i<n; i++){
-                arr[i] = ((i+1) % aux2) + 1;
-            }
-
-            // Fisher-Yates shuffle
-            for(i=1; i<(n-1); i++){
-                j = rand() % i;
-
-                // Swap
-                aux = arr[i];
-                arr[i] = arr[j];
-                arr[j] = aux;
-            }
-
-            break;
-    }
-
-    return(arr);
-}
-
-int is_sorted(int *arr, int n){
-    int i, a = 1;
-
-    for(i=0; i<(n-1); i++){
-        if(arr[i] > arr[i+1]){
-            a = 0;
-            break;
-        }
-    }
-
-    return(a);
-}
-
-double time_spent(struct timeval start, struct timeval stop){
-    double t_begin, t_end;
-
-    t_end = (double)stop.tv_usec/1000000.0 + stop.tv_sec ;
-    t_begin = (double)start.tv_usec/1000000.0 + start.tv_sec ;
-    return t_end-t_begin;
-}
 
 int main(void){
 
     int n=array_n, i, z, *arr, *arr_original;
 
-    double t_dgg=0.0, t_dgg2=0.0, t_quick=0.0;
+    double t, c;
 
     arr = (int *) calloc(n, sizeof(int));
 
-    struct timeval start, stop;
+    struct timeval time_start, time_stop;
+
+    clock_t clock_start, clock_stop;
 
     // Print header and create arrays
+    // opts (0=Sorted; 1=Random; 2=Nearly_sorted; 3=Reverse; 4=Few_unique)
+    printf("\n   USAGE EXAMPLES\n\n");
     printf("n = %d\n", n);
-    if(array_opt != 1){
-        arr_original = generate_array(n, array_opt);
-
-        switch(array_opt){
-            case 0:
-                printf("Array = Sorted\n");
-                break;
-            case 2:
-                printf("Array = Nearly sorted\n");
-                break;
-            case 3:
-                printf("Array = Reverse\n");
-                break;
-            case 4:
-                printf("Array = Few unique\n");
-                break;
-        }
-
-        printf("repeats = %d\n\n", n_arrays*repeats);
-    } else {
-        printf("Array = Random\n");
-        printf("number of arrays = %d\n", n_arrays);
-        printf("repeats = %d\n\n", repeats);
+    arr_original = generate_array(n, array_opt);
+    switch(array_opt){
+        case 0:
+            printf("opt = Sorted\n");
+            break;
+        case 1:
+            printf("opt = Random\n");
+            break;
+        case 2:
+            printf("opt = Nearly sorted\n");
+            break;
+        case 3:
+            printf("opt = Reverse\n");
+            break;
+        case 4:
+            printf("opt = Few unique\n");
+            break;
     }
 
+    printf("repeats = %d\n\n", repeats);
 
+    // #################################################################
 
-    for(z=0; z<n_arrays; z++){
-        // Create random array
-        if(array_opt==1){
-            arr_original = generate_array(n, 1);
-        }
+    // Dgg sort
 
-        // #############################################################
+    t = 0.0;
+    c = 0.0;
 
-//        // Dgg sort
-//
-//        for(i=0; i<n; i++){
-//            arr[i] = arr_original[i];
-//        }
-//
-//        gettimeofday(&start, NULL);
-//
-//        for(i=0; i<repeats; i++){
-//            dgg_sort(arr, n, 0, n-1);
-//        }
-//
-//        gettimeofday(&stop, NULL);
-//
-//        t_dgg += time_spent(start, stop);
-//
-//        if(is_sorted(arr, n) == 0){
-//            printf("\n\nUNSORTED\n\n");
-//            exit(0);
-//        }
-
-        // #############################################################
-
-        // Dgg2 sort
-
-        for(i=0; i<n; i++){
-            arr[i] = arr_original[i];
-        }
-
-        gettimeofday(&start, NULL);
-
-        for(i=0; i<repeats; i++){
-            quick_bubble_sort(arr, 0, n-1, array_cut);
-        }
-
-        gettimeofday(&stop, NULL);
-
-        t_dgg2 += time_spent(start, stop);
-
-        if(is_sorted(arr, n) == 0){
-            printf("\n\nUNSORTED\n\n");
-            exit(0);
-        }
-
-        // #############################################################
-
-        // Quick sort
-
-        for(i=0; i<n; i++){
-            arr[i] = arr_original[i];
-        }
-
-        gettimeofday(&start, NULL);
-
-        for(i=0; i<repeats; i++){
-            quick_sort(arr, 0, n-1);
-        }
-
-        gettimeofday(&stop, NULL);
-
-        t_quick += time_spent(start, stop);
-
+    for(i=0; i<n; i++){
+        arr[i] = arr_original[i];
     }
 
-//    printf("Dgg sort\n");
-//    printf("%.6f seconds or %.20f milisseconds \n\n",t_dgg,(t_dgg)*1000000);
+    gettimeofday(&time_start, NULL);
+    clock_start = clock();
+
+    for(i=0; i<repeats; i++){
+        dgg_sort(arr, n, 0, n-1);
+    }
+
+    gettimeofday(&time_stop, NULL);
+    clock_stop = clock();
+
+    if(is_sorted(arr, n) == 0){
+        printf("\n\nUNSORTED - Dgg sort\n\n");
+        exit(0);
+    }
+
+    t += time_spent(time_start, time_stop);
+    c += clock_spent(clock_start, clock_stop);
+
+    printf("Dgg sort\n");
+    printf("time: %.6f seconds or %.20f milliseconds \n", t, (t)*1000000);
+    printf("clock: %.20f milliseconds \n\n", c);
+
+    // #################################################################
+
+    // Quick bubble sort
+
+    t = 0.0;
+    c = 0.0;
+
+    for(i=0; i<n; i++){
+        arr[i] = arr_original[i];
+    }
+
+    gettimeofday(&time_start, NULL);
+    clock_start = clock();
+
+    for(i=0; i<repeats; i++){
+        quick_bubble_sort(arr, 0, n-1, array_cut);
+    }
+
+    gettimeofday(&time_stop, NULL);
+    clock_stop = clock();
+
+    if(is_sorted(arr, n) == 0){
+        printf("\n\nUNSORTED - Quick bubble sort\n\n");
+        exit(0);
+    }
+
+    t += time_spent(time_start, time_stop);
+    c += clock_spent(clock_start, clock_stop);
 
     printf("Quick bubble sort\n");
-    printf("%.6f seconds or %.20f milisseconds \n\n",t_dgg2,(t_dgg2)*1000000);
+    printf("time: %.6f seconds or %.20f milliseconds \n", t, (t)*1000000);
+    printf("clock: %.20f milliseconds \n\n", c);
+
+    // #################################################################
+
+    // Quick sort
+
+    t = 0.0;
+    c = 0.0;
+
+    for(i=0; i<n; i++){
+        arr[i] = arr_original[i];
+    }
+
+    gettimeofday(&time_start, NULL);
+    clock_start = clock();
+
+    for(i=0; i<repeats; i++){
+        quick_sort(arr, 0, n-1);
+    }
+
+    gettimeofday(&time_stop, NULL);
+    clock_stop = clock();
+
+    if(is_sorted(arr, n) == 0){
+        printf("\n\nUNSORTED - Quick sort\n\n");
+        exit(0);
+    }
+
+    t += time_spent(time_start, time_stop);
+    c += clock_spent(clock_start, clock_stop);
 
     printf("Quick sort\n");
-    printf("%.6f seconds or %.20f milisseconds \n\n",t_quick,(t_quick)*1000000);
+    printf("time: %.6f seconds or %.20f milliseconds \n", t, (t)*1000000);
+    printf("clock: %.20f milliseconds \n\n", c);
+
+    // #################################################################
 
     return 0;
 }
