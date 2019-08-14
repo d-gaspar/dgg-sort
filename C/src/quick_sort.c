e0bc1bbc317a7167fcb23d0ea7bc9012635e39e2
Daniel Gaspar Gonçalves <52671074+d-gaspar@users.noreply.github.com>
2019-08-14 15:31:29 -0300

    Update quick_sort.c

--- C/src/quick_sort.c
+++ C/src/quick_sort.c
@@ -1,4 +1,7 @@
 
+void quick_sort(int *arr, int pos1, int pos2);
+void quick_sort_VERBOSE(int *arr, int pos1, int pos2, int *iter);
+
 void quick_sort(int *arr, int pos1, int pos2){
 
     int pivo, i=pos1, j=pos2;
@@ -24,5 +27,39 @@ void quick_sort(int *arr, int pos1, int pos2){
     }
     if(j > pos1){
         quick_sort(arr, pos1, j);
+    }
+}
+
+void quick_sort_VERBOSE(int *arr, int pos1, int pos2, int *iter){
+
+    int pivo, i=pos1, j=pos2, aux;
+
+    pivo = arr[(pos1 + pos2) / 2];
+
+    while(i <= j){
+
+        if(arr[i] >= pivo || i >= pos2) iter++;
+        if(arr[j] <= pivo || j <= pos1) iter++;
+
+        while(arr[i] < pivo && i < pos2){
+            iter++;
+            i++;
+        }
+        while(arr[j] > pivo && j > pos1){
+            iter++;
+            j--;
+        }
+        if(i <= j){
+            swap(&arr[i], &arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if(i < pos2){
+        quick_sort_VERBOSE(arr, i, pos2, iter);
+    }
+    if(j > pos1){
+        quick_sort_VERBOSE(arr, pos1, j, iter);
     }
 }
